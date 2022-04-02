e390cf63a71705a9eb41efa18fa7cde28b6022b5
AktamNosirov <94_aktam@mail.ru>
2022-04-02 11:42:02 +0500

    9

--- List09.py
+++ List09.py
@@ -6,19 +6,30 @@ def main(list1):
     Returns:
         bool: return answer
     """
+    a=True
     ind=0
     b=list1[ind]
     ind+=1 
-    if list1[ind]==b and type(list1[ind])==type(b) :
-        ind+=1
     if list1[ind]==b and type(list1[ind])==type(b):
-        ind+=1
-    if list1[ind]==b and type(list1[ind])==type(b):
-        ind+=1
-    if list1[ind]==b and type(list1[ind])==type(b):
-        return True
+        pass
     else: 
-        return False
+        a=False
+    ind+=1
+    if list1[ind]==b and type(list1[ind])==type(b):
+        pass
+    else: 
+        a=False
+    ind+=1
+    if list1[ind]==b and type(list1[ind])==type(b):
+        pass
+    else: 
+        a=False
+    ind+=1
+    if list1[ind]==b and type(list1[ind])==type(b):
+        pass
+    else: 
+        a=False
+    return a
     
 list1=[1,True,1,True,1]
 print(main(list1))
